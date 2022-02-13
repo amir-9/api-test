7e4f0b318e4b0212403eff585b3e65208835fdbb
AmirHosseinHimSelf <amirr.969492@gmail.com>
2022-02-13 16:04:57 +0330

    mid of proje with lots of bugs XD!

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,77 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-data-table
+        :headers="headers"
+        :items="teams"
+        :page="page"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+        class="elevation-1"
+        @page-count="pageCount = $event"
+    ></v-data-table>
+    <div class="text-center pt-2">
+      <v-pagination
+          v-model="page"
+          :length="pageCount"
+      ></v-pagination>
+      <v-text-field
+          :value="itemsPerPage"
+          label="Items per page"
+          type="number"
+          min="-1"
+          max="15"
+          @input="itemsPerPage = parseInt($event, 10)"
+      ></v-text-field>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const axios = require('axios');
+// let dataResponse;
+let dataTeams = [];
+
+// Make a request for a user with a given ID
+const sendGetRequest = async () => {
+  try {
+    const resp = await axios.get('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=746e8490-8caf-11ec-8b49-ff10d696164c&country_id=48')
+    // console.log(resp.data)
+    dataTeams = resp.data.data
+  } catch (err) {
+    console.log(err)
+  }
+
+}
+sendGetRequest();
+
 
 export default {
+
   name: 'App',
-  components: {
-    HelloWorld
+
+  data: () => ({
+    page: 1,
+    pageCount: 0,
+    itemsPerPage: 10,
+    headers: [
+      {
+        text: 'team',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      {text: 'Calories', value: 'calories'},
+      {text: 'Fat (g)', value: 'fat'},
+      {text: 'Carbs (g)', value: 'carbs'},
+      {text: 'Protein (g)', value: 'protein'},
+      {text: 'Iron (%)', value: 'iron'},
+    ],
+    teams: dataTeams,
+  }),
+  methods: {
+    tessst() {
+      console.log(dataTeams)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './style.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const vuetify = createVuetify()
+
+app.use(vuetify,axios,VueAxios)
+
+app.mount('#app')
+
+
