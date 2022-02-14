b4c6f5c61ceb2d77349525cbddbe656f01a19041
AmirHosseinHimSelf <amirr.969492@gmail.com>
2022-02-14 12:42:00 +0330

    now getting data's from api

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <div>
-    <v-data-table
-        :headers="headers"
-        :items="teams"
-        :page="page"
-        :items-per-page="itemsPerPage"
-        hide-default-footer
-        class="elevation-1"
-        @page-count="pageCount = $event"
-    ></v-data-table>
-    <div class="text-center pt-2">
-      <v-pagination
-          v-model="page"
-          :length="pageCount"
-      ></v-pagination>
-      <v-text-field
-          :value="itemsPerPage"
-          label="Items per page"
-          type="number"
-          min="-1"
-          max="15"
-          @input="itemsPerPage = parseInt($event, 10)"
-      ></v-text-field>
-    </div>
-  </div>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th class="text-left">
+          Asset
+        </th>
+        <th class="text-left">
+          Total
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+          v-for="item in balances"
+          :key="item.asset">
+        <td>{{ item.asset }}</td>
+        <td>{{ item.total }}</td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
@@ -31,23 +28,11 @@ const axios = require('axios');
 // let dataResponse;
 let dataTeams = [];
 
-// Make a request for a user with a given ID
-const sendGetRequest = async () => {
-  try {
-    const resp = await axios.get('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=746e8490-8caf-11ec-8b49-ff10d696164c&country_id=48')
-    // console.log(resp.data)
-    dataTeams = resp.data.data
-  } catch (err) {
-    console.log(err)
-  }
-
-}
-sendGetRequest();
-
 export default {
   name: 'App',
 
   data: () => ({
+    info: [],
     page: 1,
     pageCount: 0,
     itemsPerPage: 10,
@@ -67,9 +52,21 @@ export default {
     teams: dataTeams,
   }),
   methods: {
-    tessst() {
-      console.log(dataTeams)
+    async getData() {
+      try {
+        const response = await axios.get('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=746e8490-8caf-11ec-8b49-ff10d696164c&country_id=48');
+        (response.data.data).forEach((elem)=>{
+          this.info.push(elem)
+        })
+        console.log(this.info[2]);
+      } catch (error) {
+        console.error(error);
+      }
     }
+  },
+  created() {
+    this.getData();
   }
+
 }
 </script>
