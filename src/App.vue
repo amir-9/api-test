d0d2bf1eabf370a8cba937c317feb9b578e03547
AmirHosseinHimSelf <amirr.969492@gmail.com>
2022-02-14 16:34:52 +0330

    getting and showing truley , with a loader

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,84 @@
 <template>
-  <v-simple-table dark>
-    <template v-slot:default>
-      <thead>
-      <tr>
-        <th class="text-left">
-          Asset
-        </th>
-        <th class="text-left">
-          Total
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr
-          v-for="item in balances"
-          :key="item.asset">
-        <td>{{ item.asset }}</td>
-        <td>{{ item.total }}</td>
-      </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-lazy>
+
+    <div id="app">
+
+      <v-table fixed-header
+               :height="tableHeight">
+
+        <thead>
+
+        <tr>
+          <th>#</th>
+          <th class="text-left">
+            Team Name
+          </th>
+          <th class="text-left">
+            Short Name
+          </th>
+          <th class="text-left">
+            logo
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="(team ,index) in teamData"
+            :key="team.name">
+          <td>{{ index + 1 }}</td>
+          <td>{{ team.name }}</td>
+          <td>{{ team.short_code }}</td>
+          <td><img :src="team.logo" :alt="team.short_code"></td>
+
+        </tr>
+        </tbody>
+
+      </v-table>
+      <v-progress-linear
+          width= "100vw"
+          v-if="loader"
+          indeterminate
+          color="green"
+      ></v-progress-linear>
+
+    </div>
+  </v-lazy>
+
 </template>
 
 <script>
 const axios = require('axios');
-// let dataResponse;
-let dataTeams = [];
 
 export default {
-  name: 'App',
 
-  data: () => ({
-    info: [],
-    page: 1,
-    pageCount: 0,
-    itemsPerPage: 10,
-    headers: [
-      {
-        text: 'team',
-        align: 'start',
-        sortable: false,
-        value: 'name',
-      },
-      {text: 'Calories', value: 'calories'},
-      {text: 'Fat (g)', value: 'fat'},
-      {text: 'Carbs (g)', value: 'carbs'},
-      {text: 'Protein (g)', value: 'protein'},
-      {text: 'Iron (%)', value: 'iron'},
-    ],
-    teams: dataTeams,
-  }),
+  name: 'App',
+  data() {
+    return {
+      tableHeight: '10vh',
+      teamData: null,
+      loader: true,
+    }
+  },
   methods: {
     async getData() {
       try {
-        const response = await axios.get('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=746e8490-8caf-11ec-8b49-ff10d696164c&country_id=48');
-        (response.data.data).forEach((elem)=>{
-          this.info.push(elem)
-        })
-        console.log(this.info[2]);
+        await axios.get('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=746e8490-8caf-11ec-8b49-ff10d696164c&country_id=48')
+            .then((response) => {
+              this.loader = false
+              this.teamData = response.data.data
+              this.tableHeight='100vh'
+              console.log(response.data.data)
+            });
       } catch (error) {
         console.error(error);
       }
+    },
+    say(word) {
+      console.log(word)
     }
   },
   created() {
     this.getData();
   }
-
 }
 </script>
