7f9df2a784c1ee8c26f788b33d354f3d656ad780
AmirHosseinHimSelf <amirr.969492@gmail.com>
2022-02-16 16:53:42 +0330

    pagination completed

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,8 @@
 
     <div id="app">
 
-      <v-table fixed-header
-               :height="tableHeight">
-
+      <v-table fixed-header :height="tableHeight">
         <thead>
-
         <tr>
           <th>#</th>
           <th class="text-left">
@@ -22,10 +19,9 @@
         </tr>
         </thead>
         <tbody>
-        <tr
-            v-for="(team ,index) in teamData"
+        <tr v-for="(team ,index) in someData"
             :key="team.name">
-          <td>{{ index + 1 }}</td>
+          <td>{{ ((pageNumber - 1) * itemsInPage) + (index + 1) }}</td>
           <td>{{ team.name }}</td>
           <td>{{ team.short_code }}</td>
           <td><img :src="team.logo" :alt="team.short_code"></td>
@@ -35,18 +31,71 @@
 
       </v-table>
       <v-progress-linear
-          width= "100vw"
+          width="100vw"
           v-if="loader"
           indeterminate
-          color="green"
-      ></v-progress-linear>
+          color="green">
+      </v-progress-linear>
 
+      <div class="pagination-buttons">
+        <div>
+          <button @click="pageData('first')">
+            <span> first</span>
+          </button>
+        </div>
+        <div>
+          <button @click="pageData('prev')">
+            <span><font-awesome-icon :icon="['fas','circle-chevron-left']" /></span>
+            <span> prev</span>
+          </button>
+        </div>
+        <div class="tdot">
+          <span>...</span>
+        </div>
+        <div @click="pageData('prev-3')">
+          <button>{{ pageNumber - 3 }}</button>
+        </div>
+        <div @click="pageData('prev-2')">
+          <button>{{ pageNumber - 2 }}</button>
+        </div>
+        <div @click="pageData('prev-1')">
+          <button>{{ pageNumber - 1 }}</button>
+        </div>
+        <div class="current">
+          <button>{{ pageNumber }}</button>
+        </div>
+        <div @click="pageData('next-1')">
+          <button>{{ pageNumber + 1 }}</button>
+        </div>
+        <div @click="pageData('next-2')">
+          <button>{{ pageNumber + 2 }}</button>
+        </div>
+        <div @click="pageData('next-3')">
+          <button>{{ pageNumber + 3 }}</button>
+        </div>
+        <div class="tdot">
+          <span>...</span>
+        </div>
+        <div>
+          <button @click="pageData('next')">
+            <span> next</span>
+            <span><font-awesome-icon :icon="['fas','circle-chevron-right']" /></span>
+          </button>
+        </div>
+        <div>
+          <button @click="pageData('last')">
+            <span> last</span>
+          </button>
+        </div>
+      </div>
     </div>
   </v-lazy>
 
 </template>
 
 <script>
+import './assets/css/font-awesome-all.css'
+
 const axios = require('axios');
 
 export default {
@@ -56,6 +105,10 @@ export default {
     return {
       tableHeight: '10vh',
       teamData: null,
+      someData: null,
+      currentItem: 0,
+      itemsInPage: 7,
+      pageNumber: 1,
       loader: true,
     }
   },
@@ -66,19 +119,62 @@ export default {
             .then((response) => {
               this.loader = false
               this.teamData = response.data.data
-              this.tableHeight='100vh'
-              console.log(response.data.data)
+              this.tableHeight = '93vh'
+              this.pageData('first');
             });
       } catch (error) {
         console.error(error);
       }
     },
+    pageData(item) {
+      switch (item) {
+        case 'next':
+          if (this.currentItem < this.teamData.length - this.itemsInPage) {
+            this.currentItem += this.itemsInPage;
+            this.someData = this.teamData.slice(this.currentItem, this.currentItem + this.itemsInPage);
+          }
+          break;
+        case 'prev':
+          if (this.currentItem > 0) {
+            this.currentItem -= this.itemsInPage;
+            this.someData = this.teamData.slice(this.currentItem, this.currentItem + this.itemsInPage);
+          }
+          break;
+        case 'first':
+          this.currentItem = 0;
+          this.someData = this.teamData.slice(0, this.itemsInPage);
+          break;
+        case 'last':
+          this.currentItem = Math.floor((this.teamData.length) / this.itemsInPage) * this.itemsInPage;
+          this.someData = this.teamData.slice(Math.floor((this.currentItem)), this.teamData.length);
+          break;
+        default:
+          if (item.includes('next')) {
+            if (this.currentItem < this.teamData.length - (item[5] * this.itemsInPage)) {
+              this.currentItem += item[5] * this.itemsInPage;
+              this.someData = this.teamData.slice(this.currentItem, this.currentItem + this.itemsInPage);
+            }
+          }else {
+            if (this.currentItem >= (item[5] * this.itemsInPage)) {
+              this.currentItem -= item[5] * this.itemsInPage;
+              this.someData = this.teamData.slice(this.currentItem, this.currentItem + this.itemsInPage);
+            }
+          }
+
+      }
+
+    },
     say(word) {
       console.log(word)
     }
   },
+  watch: {
+    currentItem() {
+      this.pageNumber = (this.currentItem / this.itemsInPage) + 1
+    }
+  },
   created() {
     this.getData();
-  }
+  },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import './style.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import specific icons */
+import { faCircleChevronRight, faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* add icons to the library */
+library.add(faCircleChevronRight, faCircleChevronLeft)
+
 
 const app = createApp(App)
 const vuetify = createVuetify()
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.config.productionTip = false
 
 app.use(vuetify,axios,VueAxios)
 
